eaff18a5a716b8db45fafe3486e5c42c3a14668b
RangerUFO <ufownl@gmail.com>
2025-06-23 11:08:20 +0800

    Refactor the query process

---
+++ boss_zhipin.py
@@ -0,0 +1,114 @@
+import json
+from pathlib import Path
+from urllib.parse import urlencode, quote
+from typing import Annotated, AsyncGenerator
+from playwright.async_api import BrowserContext, Page, Locator, async_playwright, expect
+from pydantic import BaseModel, Field
+
+
+base_url = "https://www.zhipin.com"
+salary_mapping = {
+    chr(0xE031): "0",
+    chr(0xE032): "1",
+    chr(0xE033): "2",
+    chr(0xE034): "3",
+    chr(0xE035): "4",
+    chr(0xE036): "5",
+    chr(0xE037): "6",
+    chr(0xE038): "7",
+    chr(0xE039): "8",
+    chr(0xE03a): "9",
+}
+
+
+async def load_cookies(context: BrowserContext, cookies_path: Path) -> None:
+    if cookies_path.exists():
+        with open(cookies_path, "r") as f:
+            data = json.load(f)
+        await context.add_cookies(data["cookies"])
+
+
+async def dump_cookies(context: BrowserContext, cookies_path: Path) -> None:
+    cookies = await context.cookies()
+    with open(cookies_path, "w") as f:
+        json.dump({"cookies": cookies}, f)
+
+
+async def login(context: BrowserContext, page: Page, cookies_path: Path) -> bool:
+    await load_cookies(context, cookies_path)
+    await page.goto(f"{base_url}/web/user/?ka=header-login", wait_until="networkidle")
+    figure = page.locator(".nav-figure")
+    for _ in range(300):
+        if await figure.is_visible():
+            await dump_cookies(context, cookies_path)
+            return True
+        try:
+            await expect(figure).to_be_visible(timeout=1000)
+        except AssertionError:
+            pass
+    return False
+
+
+def decode_salary(salary: str) -> str:
+    return "".join(salary_mapping[c] if c in salary_mapping else c for c in salary)
+
+
+class JobInfo(BaseModel):
+    name: str
+    salary: str
+    desc: str
+    url: str
+    _favor: Annotated[Locator, Field(exclude=True)]
+
+    async def favor(self):
+        await self._favor.click()
+
+
+class BossZhipin:
+    _cookies_path: Path
+
+    def __init__(self, cookies_path: str = "cookies.json"):
+        self._cookies_path = Path(cookies_path).resolve()
+
+    async def query_jobs(self, query: str, city: str, scroll_n: int) -> AsyncGenerator[JobInfo, None]:
+        async with async_playwright() as p:
+            browser = await p.chromium.launch(headless=False, args=["--disable-blink-features=AutomationControlled"])
+            context = await browser.new_context()
+            page = await context.new_page()
+            if not await login(context, page, self._cookies_path):
+                return
+            await page.goto(f"{base_url}/web/geek/jobs?{urlencode(dict(query=query, city=city), quote_via=quote)}")
+            prev_h = 0
+            container = page.locator(".job-list-container")
+            await expect(container).to_be_visible()
+            await container.hover()
+            for _ in range(scroll_n):
+                bbox = await container.bounding_box()
+                await page.mouse.wheel(0, bbox["height"] - prev_h)
+                loading = container.locator(".loading-wait")
+                try:
+                    await expect(loading).to_be_visible()
+                    await expect(loading).to_be_hidden()
+                    if bbox["height"] > prev_h:
+                        prev_h = bbox["height"]
+                    else:
+                        break
+                except AssertionError:
+                    break
+            jobs = await container.locator(".job-name").all()
+            for job in jobs:
+                await job.click()
+                jd = page.locator(".job-detail-box")
+                favor = jd.get_by_role("link", name="收藏")
+                name = jd.locator(".job-name")
+                salary = jd.locator(".job-salary")
+                desc = jd.locator(".desc")
+                await expect(desc).to_be_visible()
+                if await favor.is_visible():
+                    yield JobInfo(
+                        name = await name.inner_text(),
+                        salary = decode_salary(await salary.inner_text()),
+                        desc = await desc.inner_text(),
+                        url = await job.get_attribute("href"),
+                        _favor = favor
+                    )
--- query.py
+++ query.py
@@ -1,97 +1,6 @@
-import os
 import json
 import asyncio
-from urllib.parse import urlencode, quote
-from playwright.async_api import BrowserContext, Page, async_playwright, expect
-
-
-base_url = "https://www.zhipin.com"
-cookies_path = os.getcwd() + "/cookies.json"
-salary_mapping = {
-    chr(0xE031): "0",
-    chr(0xE032): "1",
-    chr(0xE033): "2",
-    chr(0xE034): "3",
-    chr(0xE035): "4",
-    chr(0xE036): "5",
-    chr(0xE037): "6",
-    chr(0xE038): "7",
-    chr(0xE039): "8",
-    chr(0xE03a): "9",
-}
-
-
-async def load_cookies(context: BrowserContext) -> None:
-    if os.path.exists(cookies_path):
-        with open(cookies_path, "r") as f:
-            data = json.load(f)
-        await context.add_cookies(data["cookies"])
-
-
-async def dump_cookies(context: BrowserContext) -> None:
-    cookies = await context.cookies()
-    with open(cookies_path, "w") as f:
-        json.dump({"cookies": cookies}, f)
-
-
-async def login(context: BrowserContext, page: Page) -> bool:
-    await load_cookies(context)
-    await page.goto(f"{base_url}/web/user/?ka=header-login", wait_until="networkidle")
-    figure = page.locator(".nav-figure")
-    for _ in range(300):
-        if await figure.is_visible():
-            await dump_cookies(context)
-            return True
-        try:
-            await expect(figure).to_be_visible(timeout=1000)
-        except AssertionError:
-            pass
-    return False
-
-
-def decode_salary(salary: str) -> str:
-    return "".join(salary_mapping[c] if c in salary_mapping else c for c in salary)
-
-
-async def main(query: str, city: str, scroll_n: int) -> None:
-    async with async_playwright() as p:
-        browser = await p.chromium.launch(headless=False, args=["--disable-blink-features=AutomationControlled"])
-        context = await browser.new_context()
-        page = await context.new_page()
-        if not await login(context, page):
-            return
-        await page.goto(f"{base_url}/web/geek/jobs?{urlencode(dict(query=query, city=city), quote_via=quote)}")
-        prev_h = 0
-        container = page.locator(".job-list-container")
-        await expect(container).to_be_visible()
-        await container.hover()
-        for _ in range(scroll_n):
-            bbox = await container.bounding_box()
-            await page.mouse.wheel(0, bbox["height"] - prev_h)
-            loading = container.locator(".loading-wait")
-            try:
-                await expect(loading).to_be_visible()
-                await expect(loading).to_be_hidden()
-                if bbox["height"] > prev_h:
-                    prev_h = bbox["height"]
-                else:
-                    break
-            except AssertionError:
-                break
-        jobs = await container.locator(".job-name").all()
-        for job in jobs:
-            await job.click()
-            jd = page.locator(".job-detail-box")
-            favor = jd.get_by_role("link", name="收藏")
-            name = jd.locator(".job-name")
-            salary = jd.locator(".job-salary")
-            desc = jd.locator(".desc")
-            await expect(desc).to_be_visible()
-            if await favor.is_visible():
-                print(await name.inner_text())
-                print(await job.get_attribute("href"))
-                print(decode_salary(await salary.inner_text()))
-                print(await desc.inner_text())
+from boss_zhipin import BossZhipin, JobInfo
 
 
 if __name__ == "__main__":
@@ -101,6 +10,18 @@ if __name__ == "__main__":
     cliparser.add_argument("-q", "--query", help="查询关键字", type=str, default="")
     cliparser.add_argument("--city", help="BOSS直聘城市代码 (默认: 100010000)", type=str, default="100010000")
     cliparser.add_argument("-n", "--scroll_n", help="最大滚动次数 (默认: 8)", type=int, default=8)
+    cliparser.add_argument("-O", "--output", help="输出文件路径 (默认: favor_jobs.json)", type=str, default="favor_jobs.json")
     args, _ = cliparser.parse_known_args()
 
-    asyncio.run(main(args.query, args.city, args.scroll_n))
+    async def main() -> None:
+        favor_jobs = []
+        zhipin = BossZhipin()
+        async for job in zhipin.query_jobs(args.query, args.city, args.scroll_n):
+            # TODO: 解析岗位匹配度
+            job.favor()
+            favor_jobs.append(job.model_dump())
+        if len(favor_jobs) > 0:
+            with open(args.output, "w") as f:
+                json.dump(favor_jobs, f)
+
+    asyncio.run(main())
